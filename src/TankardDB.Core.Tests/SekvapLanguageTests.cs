0a3bd83e772c6d82356797c0429eaeef5fdf62b4
sandrock <antoine.sottiau@gmail.com>
2015-08-12 13:40:27 +0200

    added SekvapLanguage

---
+++ src/TankardDB.Core.Tests/SekvapLanguageTests.cs
@@ -0,0 +1,179 @@
+﻿
+namespace TankardDB.Core.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TankardDB.Core.Internals;
+
+    [TestClass]
+    public class SekvapLanguageTests
+    {
+        [TestClass]
+        public class Ctor0
+        {
+            [TestMethod]
+            public void Works()
+            {
+                new SekvapLanguage();
+            }
+        }
+
+        [TestClass]
+        public class IsMatchMethod
+        {
+            [TestMethod]
+            public void PrefixOk_Value()
+            {
+                var lang = new SekvapLanguage();
+                string input = SekvapLanguage.Prefix + "VaLuE";
+                bool result = lang.IsMatch(input);
+                Assert.IsTrue(result, "The input should match the language processor");
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+            public void NullInput()
+            {
+                var lang = new SekvapLanguage();
+                string input = null;
+                lang.IsMatch(input);
+            }
+
+            [TestMethod]
+            public void EmptyInput()
+            {
+                var lang = new SekvapLanguage();
+                string input = string.Empty;
+                bool result = lang.IsMatch(input);
+                Assert.IsFalse(result, "The input should not match the language processor");
+            }
+
+            [TestMethod]
+            public void PrefixOk_NoValue()
+            {
+                var lang = new SekvapLanguage();
+                string input = SekvapLanguage.Prefix;
+                bool result = lang.IsMatch(input);
+                Assert.IsTrue(result, "The input should match the language processor");
+            }
+
+            [TestMethod]
+            public void PrefixOk_ValueAndStuff()
+            {
+                var lang = new SekvapLanguage();
+                string input = SekvapLanguage.Prefix + "VaLuE;Key=Val";
+                bool result = lang.IsMatch(input);
+                Assert.IsTrue(result, "The input should match the language processor");
+            }
+        }
+
+        [TestClass]
+        public class ParseMethod
+        {
+            [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+            public void NullInput()
+            {
+                var lang = new SekvapLanguage();
+                string input = null;
+                lang.Parse(input);
+            }
+
+            [TestMethod]
+            public void EmptyInput()
+            {
+                var lang = new SekvapLanguage();
+                string input = string.Empty;
+                var result = lang.Parse(input);
+                Assert.IsNull(result);
+            }
+
+            [TestMethod]
+            public void PrefixOk_NoValue()
+            {
+                var lang = new SekvapLanguage();
+                string input = SekvapLanguage.Prefix;
+                var result = lang.Parse(input);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(1, result.Count);
+                Assert.AreEqual("Value", result[0].Key);
+                Assert.AreEqual(string.Empty, result[0].Value);
+            }
+
+            [TestMethod]
+            public void PrefixOk_SimpleValue()
+            {
+                var lang = new SekvapLanguage();
+                var parts = new string[]
+                {
+                    "hello world",
+                };
+                string input = SekvapLanguage.Prefix + parts[0];
+                var result = lang.Parse(input);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(1, result.Count);
+                Assert.AreEqual("Value", result[0].Key);
+                Assert.AreEqual(parts[0], result[0].Value);
+            }
+
+            [TestMethod]
+            public void PrefixOk_SimpleValueWithEscapedSeparator()
+            {
+                var lang = new SekvapLanguage();
+                var parts = new string[]
+                {
+                    "hello ;; world",
+                };
+                string input = SekvapLanguage.Prefix + parts[0];
+                var result = lang.Parse(input);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(1, result.Count);
+                Assert.AreEqual("Value", result[0].Key);
+                Assert.AreEqual(parts[0], result[0].Value);
+            }
+
+            [TestMethod]
+            public void PrefixOk_SimpleValue_PlusOneKevap()
+            {
+                var lang = new SekvapLanguage();
+                var parts = new string[]
+                {
+                    "hello world",
+                    ";", "Name", "=", "John Smith",
+                };
+                string input = SekvapLanguage.Prefix + string.Join(string.Empty, parts);
+                var result = lang.Parse(input);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(2, result.Count);
+                int i = -1;
+                Assert.AreEqual("Value", result[++i].Key);
+                Assert.AreEqual(parts[0], result[i].Value);
+                Assert.AreEqual(parts[2], result[++i].Key);
+                Assert.AreEqual(parts[4], result[i].Value);
+            }
+
+            [TestMethod]
+            public void PrefixOk_SimpleValue_PlusTwoKevap()
+            {
+                var lang = new SekvapLanguage();
+                var parts = new string[]
+                {
+                    "hello world",
+                    ";", "Name", "=", "John Smith",
+                    ";", "Foo", "=", "Bar",
+                };
+                string input = SekvapLanguage.Prefix + string.Join(string.Empty, parts);
+                var result = lang.Parse(input);
+                Assert.IsNotNull(result);
+                Assert.AreEqual(3, result.Count);
+                int i = -1;
+                Assert.AreEqual("Value", result[++i].Key);
+                Assert.AreEqual(parts[0], result[i].Value);
+                Assert.AreEqual(parts[2], result[++i].Key);
+                Assert.AreEqual(parts[4], result[i].Value);
+                Assert.AreEqual(parts[6], result[++i].Key);
+                Assert.AreEqual(parts[8], result[i].Value);
+            }
+        }
+    }
+}
---
+++ src/TankardDB.Core/Internals/SekvapLanguage.cs
@@ -0,0 +1,117 @@
+﻿
+namespace TankardDB.Core.Internals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SekvapLanguage : IMetadataLanguage
+    {
+        public SekvapLanguage()
+        {
+        }
+
+        public bool IsMatch(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            return value != null && value.StartsWith(Prefix);
+        }
+
+        public void ResolveVariables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<KeyValuePair<string, string>> Parse(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            if (!this.IsMatch(value))
+                return null;
+
+            var result = new List<KeyValuePair<string, string>>();
+            bool isStart = true, isKey = false, isValue = false, isEnd = false;
+            string capturedKey = null;
+            int captureStartIndex = Prefix.Length, captureEndIndex, captureLength;
+            for (int i = Prefix.Length; i <= value.Length; i++)
+            {
+                char c, cp1;
+                if (i == value.Length)
+                {
+                    c = char.MinValue;
+                    cp1 = char.MinValue;
+                    isEnd = true;
+                    captureEndIndex = i - 1;
+                    captureLength = i - captureStartIndex;
+                }
+                else
+                {
+                    c = value[i];
+                    cp1 = (i+1) < value.Length ? value[i+1] : char.MinValue;
+                    captureEndIndex = i;
+                    captureLength = i - captureStartIndex;
+                }
+
+                if (isStart)
+                {
+                    if (c == ';' && cp1 == ';')
+                    {
+                        i++;
+                    }
+                    else if (c == ';' && cp1 != ';' || isEnd)
+                    {
+                        // end of start part
+                        AddToResult(result, "Value", value.Substring(captureStartIndex, captureLength));
+                        i++;
+                        isStart = false;
+                        isKey = true;
+                        captureStartIndex = i;
+                    }
+                }
+                else if (isKey)
+                {
+                    if (c == '=' && cp1 == '=')
+                    {
+                        i++;
+                    }
+                    else if (c == '=' && cp1 != '=' || isEnd)
+                    {
+                        // end of start part
+                        capturedKey = value.Substring(captureStartIndex, captureLength);
+                        i++;
+                        isKey = false;
+                        isValue = true;
+                        captureStartIndex = i;
+                    }
+                }
+                else if (isValue)
+                {
+                    if (c == ';' && cp1 == ';')
+                    {
+                        i++;
+                    }
+                    else if (c == ';' && cp1 != ';' || isEnd)
+                    {
+                        // end of start part
+                        var capturedValue = value.Substring(captureStartIndex, captureLength);
+                        AddToResult(result, capturedKey, capturedValue);
+                        i++;
+                        isStart = false;
+                        isKey = true;
+                        captureStartIndex = i;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static void AddToResult(List<KeyValuePair<string, string>> collection, string key, string value)
+        {
+            collection.Add(new KeyValuePair<string, string>(key, value));
+        }
+    }
+}
